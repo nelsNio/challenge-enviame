72de102248776ce8c31a6a92a660cd75f0fc884f
Andres  Niño <30846266+nelsNio@users.noreply.github.com>
2021-05-08 13:20:51 -0500

    Ejercicio 2: API REST + CRUD
    Dentro del ambiente dockerizado desarrolla una API Rest, con el stack de tu preferencia, que implemente un CRUD de una entidad tipo 'empresa'. Preocupate de incluir un script que genere N registros con datos "fake" (utilizando una librería faker).

--- empresa/views.py
+++ empresa/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from empresa.models import Empresa
 from empresa.serializers import EmpresaSerializer
+from faker import Faker
+
+
 
 class EmpresaViewset(viewsets.ModelViewSet):
     queryset=Empresa.objects.all().order_by('-id')
     serializer_class=EmpresaSerializer
+
+
+    @action(methods=['POST'],detail=False)
+    def fill_data(self, pk=None):
+        print('fill_data in entity empresa')
+        fake = Faker()
+        for _ in range(10):
+            name=fake.name()
+            address=fake.address()
+            print(name,"  ", address)
+            serializer= EmpresaSerializer(data={'name':name,'address':address})
+            if serializer.is_valid(raise_exception=True):
+                Empresa(**serializer.validated_data).save()
+
+
+        return Response({'ok':True}) 
